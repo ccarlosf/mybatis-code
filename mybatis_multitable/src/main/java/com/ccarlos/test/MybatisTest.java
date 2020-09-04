7d69a6d97b70f65511631a80bbd0bbf92bf32707
ccarlosf <2238513100@qq.com>
2020-09-04 10:32:48 +0800

    第五部分 Mybatis复杂映射开发 5.1 一对一查询

---
+++ mybatis_multitable/src/main/java/com/ccarlos/test/MybatisTest.java
@@ -0,0 +1,33 @@
+package com.ccarlos.test;
+
+import com.ccarlos.mapper.IOrderMapper;
+import com.ccarlos.pojo.Order;
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+import org.junit.Test;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.List;
+
+public class MybatisTest {
+
+    @Test
+    public void test1() throws IOException {
+        InputStream resourceAsStream = Resources.getResourceAsStream("sqlMapConfig.xml");
+        SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(resourceAsStream);
+        SqlSession sqlSession = sqlSessionFactory.openSession();
+        IOrderMapper mapper = sqlSession.getMapper(IOrderMapper.class);
+        List<Order> orderAndUser = mapper.findAll();
+        for (Order order : orderAndUser) {
+            System.out.println(order);
+        }
+
+    }
+
+}
+
+
+
